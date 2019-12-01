7eca4984bc93acf0c500128419350e4afc444abe
cool3690 <cool3690@yahoo.com.tw>
2019-12-01 20:24:53 +0800

    Add files via upload

--- cont.php
+++ cont.php
@@ -24,10 +24,8 @@ foreach($statement as $row){
 ?>
 
     <body>
-       
-  
-        
-    <div class="container">
+        <font size="7"><center>成績</font> </br> </br>
+    <div class="container"><form action="cont.php" method="post">
 		<table class="table table-striped table-responsive-xl">
 				
 				<tbody>			      
@@ -53,6 +51,8 @@ foreach($statement as $row){
 				</tr> 
 				</tbody>
 			</table>
+				<center><input type="submit" class="btn btn-primary" value="送出">
+			</form>
          </div>
     </body>
 </html>
