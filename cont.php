c528dec2e1e59e09071bbb1953634f73e6750ed0
cool3690 <cool3690@yahoo.com.tw>
2019-12-03 15:00:25 +0800

    Add files via upload

--- cont.php
+++ cont.php
@@ -12,21 +12,18 @@
   <script type="text/javascript" src="jquery/jquery-ui-timepicker-zh-TW.js"></script>
     <link rel="stylesheet" href="https://cdn.rawgit.com/dmuy/MDTimePicker/7d5488f/mdtimepicker.min.css">
     <script src="https://cdn.rawgit.com/dmuy/MDTimePicker/7d5488f/mdtimepicker.min.js"></script>
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
     </head>
 <?php
 require_once("db.php");
-/*
-$statement = $db->query('select userid,rawgrade,rawgrademax from mdl_grade_grades');
-
-foreach($statement as $row){
-    echo $row['userid']." ".$row['rawgrade']." ".$row['rawgrademax']."<br>";
-}
-  */
 ?>
 
+
     <body>
+	 
         <font size="7"><center>成績</font> </br> </br>
-    <div class="container"><form action="cont.php" method="post">
+    <div class="container">
+	<form action="cont.php" method="post">
 		<table class="table table-striped table-responsive-xl">
 				
 				<tbody>			      
@@ -36,11 +33,10 @@ foreach($statement as $row){
 					 <select name="emp2">	                
 					
 	<?php
-
-		$statement = $db->query('SELECT username,firstname,lastname FROM mdl_user');
+		$statement = $db->query('SELECT id, username,firstname,lastname FROM mdl_user');
 		foreach($statement as $row){
 			?>
-			 <option value="<?= $row['username']?>"><?=$row['lastname'].$row['firstname']?></option>
+			 <option value="<?= $row['id']?>"><?=$row['lastname'].$row['firstname']?></option>
 		   
 		<?php
  
@@ -52,8 +48,54 @@ foreach($statement as $row){
 				</tr> 
 				</tbody>
 			</table>
-				<center><input type="submit" class="btn btn-primary" value="送出">
+				<center><input type="submit" class="btn btn-primary" value="送出"><br>
 			</form>
+			<br><br><br>
+				<table class="table table-striped table-responsive-xl">
+				<tbody>	
+<tr>				
+			<?php
+			$i=0;
+if($_POST['emp2']!=null){
+	$sub=$_POST['emp2'];
+$grade= "['time','rawgrade'],";
+$statement = $db->query("select userid,rawgrade,rawgrademax,timecreated from mdl_grade_grades where userid='$sub' and timecreated!='NULL'");
+//$statement->bindParam(':name', $sub); 
+ echo "<tr><th>次數</th><th>考試成績  /  總分</th></tr>";
+foreach($statement as $row){
+	$i++;
+	
+    echo "<tr><td>". $i."</td><td>".round($row['rawgrade'],2)."   /  ".round($row['rawgrademax'],2)."</td></tr>";
+	$g=$row['rawgrade'];
+	
+	
+	$grade.="['$i',$g],";
+}
+$grade=rtrim($grade,",");
+}
+
+/**/
+
+  
+?>
+                                     
+				</tr> 
+				</tbody>
+			</table>
+	<script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable(<?php echo "[$grade]"?>);     
+        var options = {title: '成績', backgroundColor:'yellow'};
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+	  google.setOnLoadCallback(drawChart1);
+	  
+    </script>
+			<div id="chart_div" style="width: 900px; height: 200px;"></div>
+			 
          </div>
     </body>
 </html>
