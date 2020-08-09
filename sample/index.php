f801fe3da4358925e50ec9d224b9d85e8e9de9c7
cool3690 <cool3690@yahoo.com.tw>
2020-08-09 22:11:24 +0800

    Update index.php
    0809

--- sample/index.php
+++ sample/index.php
@@ -1,15 +1,18 @@
 <?php
 //'6FHzChshWrTfDVs-z7s',
-//'2QZ72aAJQsLmIMd2ILs'
+//'2QZ72aAJQsLmIMd2ILs' 'https://cloud.scorm.com/lrs/SF85CK9136/',
 $loader = require '../vendor/autoload.php';
 
 $lrs = new TinCan\RemoteLRS(
-    'https://cloud.scorm.com/lrs/SF85CK9136/',
+   
+	'https://cloud.scorm.com/lrs/SF85CK9136/',
     '1.0.0',
     '6FHzChshWrTfDVs-z7s',
     '2QZ72aAJQsLmIMd2ILs'
 );
-$response = $lrs->queryStatements(['limit' =>3]);
+
+//$response = $lrs->queryStatements(['limit' =>20]);
+$response = $lrs->queryStatements(['verb' =>"completed"]);
 $test2 = json_encode($response, JSON_PRETTY_PRINT);
 HEADER('Content-Type: application/json; charset=utf-8');
 $json = json_decode($test2,true);
@@ -21,11 +24,20 @@ $json = json_decode($test2,true);
   // print_r($test3["more"]);
   
  $test4= json_decode($test3, true);
- print_r($json_data   );
+  
+  print_r($test4   );
+for($i=0;$i<19;$i++){
+	if(!$test4 ["statements"][$i]["actor"]["name"]){
+		break;
+	}
+	else{
+		print_r($test4 ["statements"][$i]["actor"]["name"] ." ");
+	}
+}
 
-print_r($test4 ["statements"][0]["id"] );
 
-
+print_r($test4 ["statements"][10]["result"]["score"]["raw"] ."  ");
+	 
 
 /*  foreach($json as $value) {
 	 		  ;
@@ -34,9 +46,5 @@ print_r($test4 ["statements"][0]["id"] );
     }
 	 
 */
- // var_dump($test2);
-//$debug = var_export($test2, true);
- 
-// echo $json->{'httpResponse'};
 
 ?>
