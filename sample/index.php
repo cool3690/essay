de7e5816d41f89f2e92b1900b7c4c10a5e5d612c
cool3690 <cool3690@yahoo.com.tw>
2020-08-13 15:49:17 +0800

    Update index.php
    0813

--- sample/index.php
+++ sample/index.php
@@ -1,50 +1,58 @@
 <?php
 //'6FHzChshWrTfDVs-z7s',
-//'2QZ72aAJQsLmIMd2ILs' 'https://cloud.scorm.com/lrs/SF85CK9136/',
+//'2QZ72aAJQsLmIMd2ILs'
 $loader = require '../vendor/autoload.php';
 
 $lrs = new TinCan\RemoteLRS(
-   
-	'https://cloud.scorm.com/lrs/SF85CK9136/',
+    'https://cloud.scorm.com/lrs/SF85CK9136/',
     '1.0.0',
     '6FHzChshWrTfDVs-z7s',
     '2QZ72aAJQsLmIMd2ILs'
 );
+$actor = new TinCan\Agent(
+    [ 'name' => '名 小' ]
+);
+$verb = new TinCan\Verb(
+    [ 'id' => 'http://adlnet.gov/expapi/verbs/completed' ]
+);
+$activity = new TinCan\Activity(
+    [ 'id' => 'http://rusticisoftware.github.com/TinCanPHP' ]
+);
+$statement = new TinCan\Statement(
+    [
+         
+        'verb'  => $verb,
+         
+    ]
+);
+$result =  new TinCan\Result(
+    [
+         "success" => true,
+         
+         
+    ]
+);
 
-//$response = $lrs->queryStatements(['limit' =>20]);
-$response = $lrs->queryStatements(['verb' =>"completed"]);
+ $response = $lrs->queryStatements(
+			[ 'actor'  =>$actor ,
+			  'verb'  => $verb,
+			  'result' => $result
+			]
+ 
+			);
+		
 $test2 = json_encode($response, JSON_PRETTY_PRINT);
 HEADER('Content-Type: application/json; charset=utf-8');
 $json = json_decode($test2,true);
-
+	
 
   $json_data = json_decode($test2, true);
-   //  print_r($json_data );
+     
    $test3=$json_data["httpResponse"]["_content"];
-  // print_r($test3["more"]);
+  
   
  $test4= json_decode($test3, true);
-  
-  print_r($test4   );
-for($i=0;$i<19;$i++){
-	if(!$test4 ["statements"][$i]["actor"]["name"]){
-		break;
-	}
-	else{
-		print_r($test4 ["statements"][$i]["actor"]["name"] ." ");
-	}
-}
+  print_r($test4 );
 
-
-print_r($test4 ["statements"][10]["result"]["score"]["raw"] ."  ");
-	 
-
-/*  foreach($json as $value) {
-	 		  ;
-       // echo  $value .'<br><br>';  
-	
-    }
-	 
-*/
-
+//print_r($test4 ["statements"][0]["id"] );
 ?>
